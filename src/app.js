7adb41bedc4f707dee59998ac1345408cbecff59
AndreNunes1812 <andre.nunes0207700@gmail.com>
2020-04-12 14:00:48 +0200

    atualização dos verbos GET,POST,PUT e DELETE

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,93 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.status(200).json(repositories)
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes : 0
+  }
+
+  repositories.push(repositorie);
+
+  return response.status(200).json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+ 
+  const repositorieIndex = repositories.findIndex( rep => rep.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found!'})
+  }
+  
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes:repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repo
+
+  return response.status(200).json(repo)
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex( rep => rep.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found!'})
+  }
+  
+  repositories.splice(repositorieIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex( rep => rep.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found!'})
+  }
+  
+  const repos = repositories[repositorieIndex]
+
+  const newRepositorie = {
+    id,
+    title: repos.title,
+    url: repos.url,
+    techs:repos.techs,
+    likes: repos.likes + 1
+  }
+
+  repositories[repositorieIndex] = newRepositorie
+
+  return response.status(200).json(newRepositorie)
+ 
 });
 
 module.exports = app;
